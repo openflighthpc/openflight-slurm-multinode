59eeb19c9649424dc2c8526a9f962772d5c6d409
Stu Franks <stu@stuts.uk>
2022-11-01 16:11:18 +0000

    Add flight-pdsh to prepare

--- prepare.sh
+++ prepare.sh
@@ -1,3 +1,6 @@
+# PDSH package
+yum install -y flight-pdsh
+
 # SLURM & Munge Packages
 yum install -y munge munge-libs perl-Switch numactl flight-slurm flight-slurm-devel flight-slurm-perlapi flight-slurm-torque flight-slurm-slurmd flight-slurm-example-configs flight-slurm-libpmi flight-slurm-slurmctld
 

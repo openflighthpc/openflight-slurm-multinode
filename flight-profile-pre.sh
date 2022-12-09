4d9f0c00c23220fbca272a544ff0ac9c814d5f2e
Stu Franks <stu@stuts.uk>
2022-12-09 15:25:51 +0000

    Add profile pre script to allow add nodes to work

---
+++ flight-profile-pre.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+SLURM_SERVER=$((sudo -i /opt/flight/bin/flight hunter show $SLURM_SERVER --plain 2>/dev/null | awk 'END {if (NR == 0) exit 1 }') && echo $(sudo -i /opt/flight/bin/flight hunter show $SLURM_SERVER --plain |awk '{print $2}') || echo $SLURM_SERVER)
+
+ansible-playbook -i $INVFILE --extra-vars="cluster_name=$CLUSTERNAME nfs_server=$NFS_SERVER slurm_server=$SLURM_SERVER default_username=$DEFAULT_USERNAME default_user_password=$DEFAULT_PASSWORD access_host=$ACCESS_HOST compute_ip_range=$COMPUTE_IP_RANGE hunter_hosts=$HUNTER_HOSTS" --tags pre $RUN_ENV/openflight-slurm-multinode/main.yml

23d72ba7efb614c04c16c02668b03f897b6d048f
Stu Franks <stu@stuts.uk>
2023-09-08 11:35:44 +0100

    Add IPA deps to prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -6,3 +6,9 @@ yum install -y munge munge-libs perl-Switch numactl flight-slurm flight-slurm-de
 
 # Install NFS Packages
 yum install -y nfs-utils
+
+# IPA 
+dnf module reset -y idm
+dnf module enable -y idm:DL1
+dnf module install -y idm:DL1/dns
+ansible-galaxy collection install 'community.general:>6.6.0' --upgrade

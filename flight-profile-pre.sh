51d944e376d37d938995f86ded396331b782c470
Stu Franks <stu@stuts.uk>
2024-03-12 15:52:27 +0000

    Add NFS server back into profile scripts

--- flight-profile-pre.sh
+++ flight-profile-pre.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -i $INVFILE --extra-vars="cluster_name=$CLUSTERNAME default_username=$DEFAULT_USERNAME default_user_password=$DEFAULT_PASSWORD access_host=$ACCESS_HOST compute_ip_range=$COMPUTE_IP_RANGE hunter_hosts=$HUNTER_HOSTS ipa_use=$IPA_USE ipa_domain=$IPA_DOMAIN secure_admin_password=$SECURE_ADMIN_PASSWORD" --tags pre $RUN_ENV/openflight-slurm-multinode/main.yml
+ansible-playbook -i $INVFILE --extra-vars="cluster_name=$CLUSTERNAME default_username=$DEFAULT_USERNAME default_user_password=$DEFAULT_PASSWORD access_host=$ACCESS_HOST nfs_server=$NFS_SERVER compute_ip_range=$COMPUTE_IP_RANGE hunter_hosts=$HUNTER_HOSTS ipa_use=$IPA_USE ipa_domain=$IPA_DOMAIN secure_admin_password=$SECURE_ADMIN_PASSWORD" --tags pre $RUN_ENV/openflight-slurm-multinode/main.yml

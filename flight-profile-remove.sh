a48324a9e8b461283cb0206b9496460d00850070
Stu Franks <stu@stuts.uk>
2024-03-12 16:13:14 +0000

    Rename nfs_server to default_nfs_server

--- flight-profile-remove.sh
+++ flight-profile-remove.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ansible-playbook -i $INVFILE --extra-vars="cluster_name=$CLUSTERNAME default_username=$DEFAULT_USERNAME default_user_password=$DEFAULT_PASSWORD access_host=$ACCESS_HOST nfs_server=$NFS_SERVER compute_ip_range=$COMPUTE_IP_RANGE hunter_hosts=$HUNTER_HOSTS ipa_use=$IPA_USE ipa_domain=$IPA_DOMAIN secure_admin_password=$SECURE_ADMIN_PASSWORD" --extra-vars="{"remove_node": [$NODE]}" --tags remove $RUN_ENV/openflight-slurm-multinode/remove.yml
+ansible-playbook -i $INVFILE --extra-vars="cluster_name=$CLUSTERNAME default_username=$DEFAULT_USERNAME default_user_password=$DEFAULT_PASSWORD access_host=$ACCESS_HOST default_nfs_server=$NFS_SERVER compute_ip_range=$COMPUTE_IP_RANGE hunter_hosts=$HUNTER_HOSTS ipa_use=$IPA_USE ipa_domain=$IPA_DOMAIN secure_admin_password=$SECURE_ADMIN_PASSWORD" --extra-vars="{"remove_node": [$NODE]}" --tags remove $RUN_ENV/openflight-slurm-multinode/remove.yml
